4f95c9d9dbccc96927d06ba3d3ff4bdde7d10b0f
Matthew Volk <volkmattj@gmail.com>
2020-11-12 15:20:34 -0600

    connect to mongodb

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Successfully connected to MongoDB"))
+  .catch((err) => {
+    console.log(`MongoDB Connection Error: ${err.message}`);
+  });
 
 app.use(express.static(path.join(__dirname, "client/build")));
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
