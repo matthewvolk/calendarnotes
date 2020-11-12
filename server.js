086538678ae68390b03215922a428901337a63bc
Matthew Volk <volkmattj@gmail.com>
2020-11-12 07:06:40 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("/api", (req, res) => {
+  res.json({ msg: "Hello, Calendar Notes!" });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`HTTP server listening on http://localhost:${port}`);
+});
