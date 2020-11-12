1634a865559b48d80fe8f7d2940ff789de650d96
Matthew Volk <volkmattj@gmail.com>
2020-11-12 08:02:55 -0600

    add app name, version, author

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  state = { msg: null };
+  state = { data: {} };
 
   componentDidMount() {
     this.getMessage();
@@ -12,17 +12,24 @@ class App extends Component {
   getMessage = () => {
     fetch("/api")
       .then((res) => res.json())
-      .then((data) => this.setState({ msg: data.msg }));
+      .then((data) => this.setState({ data }));
   };
 
   render() {
-    const { msg } = this.state;
+    const { data } = this.state;
+    console.log(data);
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>{msg}</p>
+          <h3 style={{ margin: "0.25rem 0 1rem 0" }}>{data.name}</h3>
+          <pre style={{ margin: "0 0 0.25rem 0", fontSize: "1.15rem" }}>
+            v{data.version}
+          </pre>
+          <pre style={{ margin: "0", fontSize: "1.15rem" }}>
+            Author: {data.author}
+          </pre>
         </header>
       </div>
     );
--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/api", (req, res) => {
-  res.json({ msg: "Hello, Calendar Notes!" });
+  res.json({
+    name: "CalendarNotes",
+    version: "0.0.1 alpha",
+    author: "Matthew Volk",
+  });
 });
 
 app.get("*", (req, res) => {
