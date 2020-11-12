9d9d103a8480e25bfc4c0bcd80843f019fc8a25c
Matthew Volk <volkmattj@gmail.com>
2020-11-12 10:19:24 -0600

    add body parser

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ require("dotenv").config();
 const path = require("path");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "client/build")));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/api", (req, res) => {
   res.json({
