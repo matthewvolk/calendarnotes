c4fe88f4e398d868322a2ebaf9d5f6c0651d26df
Matthew Volk <volkmattj@gmail.com>
2020-12-27 19:34:16 -0600

    refactor authentication handling in react

---
+++ client/src/context/Auth.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const AuthContext = React.createContext();
+
+function AuthProvider({ children }) {
+  const [state, setState] = React.useState({
+    status: "pending",
+    error: null,
+    user: null,
+  });
+  React.useEffect(() => {
+    fetch("https://localhost:8443/api/user", { credentials: "include" })
+      .then((response) => {
+        if (response.status === 401) {
+          setState({ status: "success", error: null, user: null });
+        } else {
+          return response.json();
+        }
+      })
+      .then((user) => setState({ status: "success", error: null, user }))
+      .catch((error) => setState({ status: "error", error, user: null }));
+  }, []);
+
+  return (
+    <AuthContext.Provider value={state}>
+      {state.status === "pending" ? (
+        <div>Loading...</div>
+      ) : state.status === "error" ? (
+        <div>
+          Error!
+          <div>
+            <pre>{state.error.message}</pre>
+          </div>
+        </div>
+      ) : (
+        children
+      )}
+    </AuthContext.Provider>
+  );
+}
+
+// useAuthState can only be called from within <AuthProvider />
+function useAuthState() {
+  const state = React.useContext(AuthContext);
+  const isPending = state.status === "pending";
+  const isError = state.status === "error";
+  const isSuccess = state.status === "success";
+  const isAuthenticated = state.user && isSuccess;
+  return {
+    ...state,
+    isPending,
+    isError,
+    isSuccess,
+    isAuthenticated,
+  };
+}
+
+export { AuthProvider, useAuthState };
