4a75a540b2ca13e680f33856654d73c8c3b8ec9c
Matthew Volk <volkmattj@gmail.com>
2021-04-26 09:56:28 -0500

    modify debugging statement

--- services/UserService.js
+++ services/UserService.js
@@ -181,7 +181,7 @@ class UserService {
       } else {
         console.error(
           "Call to get calendar events in getCalendarEvents() failed for some other reason than 401",
-          err.response.data.errors
+          err.response.data.error
         );
       }
     }
