bff4cd830a82c4fdad9e361ed30d9cbf5b225896
Matthew Volk <volkmattj@gmail.com>
2021-04-29 15:06:56 -0500

    allow user to create google doc notes in google drive

--- services/UserService.js
+++ services/UserService.js
@@ -3,6 +3,7 @@ const DateService = require("./DateService");
 const axios = require("axios");
 const moment = require("moment-timezone");
 const { differenceInMinutes, parseISO, format } = require("date-fns");
+const { DateTime } = require("luxon");
 
 const logAxiosErrors = (err) => {
   if (err.response) {
@@ -331,7 +332,6 @@ class UserService {
           childFolders = childFolders.map((folder, index) => {
             return { ...folder, hasChildFolders: folderHasChildren[index] };
           });
-          console.log("childFolders:", childFolders);
           return childFolders;
         }
       } catch (err) {
@@ -390,7 +390,6 @@ class UserService {
               childFolders = childFolders.map((folder, index) => {
                 return { ...folder, hasChildFolders: folderHasChildren[index] };
               });
-              console.log("childFolders:", childFolders);
               return childFolders;
             }
           } catch (err) {
@@ -843,10 +842,9 @@ class UserService {
           attendees: eventResponse.data.attendees,
           organizer: eventResponse.data.organizer,
         };
-        console.log(eventResponse);
       } catch (err) {
         if (err.response && err.response.status === 401) {
-          userWithRefreshedToken = await this.refreshToken(user, "GOOGLE");
+          let userWithRefreshedToken = await this.refreshToken(user, "GOOGLE");
 
           try {
             eventResponse = await axios({
@@ -871,14 +869,326 @@ class UserService {
         }
       }
 
-      console.log(eventData);
-
       // Create Google Doc and fill it out with Calendar Event info
       let googleDoc = {
-        title: eventData.title,
+        title: `${eventData.title} - ${DateTime.fromISO(
+          eventData.start.dateTime,
+          { setZone: true }
+        ).toFormat("cccc, LLLL d ⋅ t")} - ${DateTime.fromISO(
+          eventData.end.dateTime,
+          { setZone: true }
+        ).toFormat("t")}`,
       };
+
       // Save Google Doc to folderId
+      let googleDocResponse;
+      try {
+        googleDocResponse = await axios({
+          method: "post",
+          url: "https://docs.googleapis.com/v1/documents",
+          data: googleDoc,
+          headers: {
+            Authorization: `Bearer ${user.googleDrive.accessToken}`,
+          },
+        });
+      } catch (err) {
+        if (err.response && err.response.status === 401) {
+          let userWithRefreshedToken = await this.refreshToken(user, "GOOGLE");
+          try {
+            googleDocResponse = await axios({
+              method: "post",
+              url: "https://docs.googleapis.com/v1/documents",
+              data: googleDoc,
+              headers: {
+                Authorization: `Bearer ${userWithRefreshedToken.googleDrive.accessToken}`,
+              },
+            });
+          } catch (err) {
+            logAxiosErrors(err);
+          }
+        } else {
+          logAxiosErrors(err);
+        }
+      }
+
+      const docId = googleDocResponse.data.documentId;
+
+      let googleDocFileInfoResponse;
+      try {
+        googleDocFileInfoResponse = await axios({
+          method: "get",
+          url: `https://www.googleapis.com/drive/v3/files/${docId}?fields=${encodeURIComponent(
+            "id, name, webViewLink, parents"
+          )}`,
+          headers: {
+            Authorization: `Bearer ${user.googleDrive.accessToken}`,
+          },
+        });
+      } catch (err) {
+        if (err.response && err.response.status === 401) {
+          let userWithRefreshedToken = await this.refreshToken(
+            user,
+            "GOOGLE",
+            "DRIVE"
+          );
+
+          try {
+            googleDocFileInfoResponse = await axios({
+              method: "get",
+              url: `https://www.googleapis.com/drive/v3/files/${docId}?fields=${encodeURIComponent(
+                "id, name, webViewLink, parents"
+              )}`,
+              headers: {
+                Authorization: `Bearer ${userWithRefreshedToken.googleDrive.accessToken}`,
+              },
+            });
+          } catch (err) {
+            logAxiosErrors(err);
+          }
+        } else {
+          logAxiosErrors(err);
+        }
+      }
+
+      const googleDocFileInfo = googleDocFileInfoResponse.data;
+      const parents = googleDocFileInfoResponse.data.parents;
+
+      let moveGoogleDocResponse;
+      try {
+        moveGoogleDocResponse = await axios({
+          method: "patch",
+          url: `https://www.googleapis.com/drive/v3/files/${googleDocFileInfo.id}?removeParents=${parents[0]}&addParents=${folderId}`,
+          headers: {
+            Authorization: `Bearer ${user.googleDrive.accessToken}`,
+          },
+        });
+      } catch (err) {
+        if (err.response && err.response.status === 401) {
+          let userWithRefreshedToken = await this.refreshToken(
+            user,
+            "GOOGLE",
+            "DRIVE"
+          );
+
+          try {
+            moveGoogleDocResponse = await axios({
+              method: "patch",
+              url: `https://www.googleapis.com/drive/v3/files/${googleDocFileInfo.id}?removeParents=${parents[0]}&addParents=${folderId}`,
+              headers: {
+                Authorization: `Bearer ${userWithRefreshedToken.googleDrive.accessToken}`,
+              },
+            });
+          } catch (err) {
+            logAxiosErrors(err);
+          }
+        } else {
+          logAxiosErrors(err);
+        }
+      }
+
+      // Update Google Doc Body
+      // let googleDocContentRequests = [];
+      // googleDocContentRequests.push({
+      //   insertText: {
+      //     text: "Attendees:",
+      //     location: {
+      //       index: 1,
+      //     },
+      //   },
+      // });
+      // googleDocContentRequests.push({
+      //   updateParagraphStyle: {
+      //     range: {
+      //       startIndex: 1,
+      //       endIndex: 12,
+      //     },
+      //     paragraphStyle: {
+      //       namedStyleType: "HEADING_1",
+      //     },
+      //     fields: "*",
+      //   },
+      // });
+      // googleDocContentRequests.push({
+      //   insertSectionBreak: {
+      //     sectionType: "CONTINUOUS",
+      //     location: {
+      //       index: 11,
+      //     },
+      //   },
+      // });
+      // googleDocContentRequests.push({
+      //   updateParagraphStyle: {
+      //     fields: "*",
+      //     range: {
+      //       startIndex: 13,
+      //       endIndex: 14,
+      //     },
+      //     paragraphStyle: {
+      //       namedStyleType: "NORMAL_TEXT",
+      //     },
+      //   },
+      // });
+      // googleDocContentRequests.push({
+      //   insertText: {
+      //     text: `${eventData.organizer.email}\n`,
+      //     location: {
+      //       index: 13,
+      //     },
+      //   },
+      // });
+      // let organizerEmailEndIndex =
+      //   eventData.organizer.email.length + 13 + 2 + 1;
+      // googleDocContentRequests.push({
+      //   updateTextStyle: {
+      //     fields: "*",
+      //     range: {
+      //       startIndex: 13,
+      //       endIndex: organizerEmailEndIndex,
+      //     },
+      //     textStyle: {
+      //       link: {
+      //         url: `mailto:${eventData.organizer.email}`,
+      //       },
+      //       foregroundColor: {
+      //         color: {
+      //           rgbColor: {
+      //             red: 0,
+      //             green: 0.47,
+      //             blue: 0.81,
+      //           },
+      //         },
+      //       },
+      //       underline: true,
+      //     },
+      //   },
+      // });
+      // googleDocContentRequests.push({
+      //   createParagraphBullets: {
+      //     bulletPreset: "BULLET_DISC_CIRCLE_SQUARE",
+      //     range: {
+      //       startIndex: 13,
+      //       endIndex: organizerEmailEndIndex,
+      //     },
+      //   },
+      // });
+
+      // googleDocContentRequests.push({
+      //   insertText: {
+      //     text: `${eventData.attendees[0].email}\n`,
+      //     location: {
+      //       index: organizerEmailEndIndex - 1,
+      //     },
+      //   },
+      // });
+
+      // let googleDocContentResponse;
+      // try {
+      //   googleDocContentResponse = await axios({
+      //     method: "post",
+      //     url: `https://docs.googleapis.com/v1/documents/${googleDocFileInfo.id}:batchUpdate`,
+      //     data: {
+      //       requests: googleDocContentRequests,
+      //     },
+      //     headers: {
+      //       Authorization: `Bearer ${user.googleDrive.accessToken}`,
+      //     },
+      //   });
+      // } catch (err) {
+      //   if (err.response && err.response.status === 401) {
+      //     let userWithRefreshedToken = await this.refreshToken(
+      //       user,
+      //       "GOOGLE",
+      //       "DRIVE"
+      //     );
+      //     try {
+      //       googleDocContentResponse = await axios({
+      //         method: "post",
+      //         url: `https://docs.googleapis.com/v1/documents/${googleDocFileInfo.id}:batchUpdate`,
+      //         data: {
+      //           requests: googleDocContentRequests,
+      //         },
+      //         headers: {
+      //           Authorization: `Bearer ${userWithRefreshedToken.googleDrive.accessToken}`,
+      //         },
+      //       });
+      //     } catch (err) {
+      //       logAxiosErrors(err);
+      //     }
+      //   } else {
+      //     logAxiosErrors(err);
+      //   }
+      // }
+
+      // console.log(googleDocContentResponse);
+
       // Create Google Calendar _Notes event
+      let googleEventCreationResponse;
+
+      try {
+        googleEventCreationResponse = await axios({
+          method: "post",
+          url: `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`,
+          headers: {
+            Authorization: `Bearer ${user.google.accessToken}`,
+          },
+          data: {
+            summary: "_Notes",
+            description: googleDocFileInfo.webViewLink,
+            start: {
+              dateTime: eventData.start.dateTime,
+            },
+            end: {
+              dateTime: eventData.end.dateTime,
+            },
+            reminders: {
+              useDefault: false,
+            },
+            colorId: "8",
+          },
+        });
+        console.log("Created Google Calendar Notes Task!");
+        return { status: 200, message: "Success!" };
+      } catch (err) {
+        if (err.response && err.response.status === 401) {
+          userWithRefreshedToken = await this.refreshToken(user, "GOOGLE");
+
+          try {
+            googleEventCreationResponse = await axios({
+              method: "post",
+              url: `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`,
+              headers: {
+                Authorization: `Bearer ${userWithRefreshedToken.google.accessToken}`,
+              },
+              data: {
+                summary: "_Notes",
+                description: googleDocFileInfo.webViewLink,
+                start: {
+                  dateTime: eventData.start.dateTime,
+                },
+                end: {
+                  dateTime: eventData.end.dateTime,
+                },
+                reminders: {
+                  useDefault: false,
+                },
+                colorId: "8",
+              },
+            });
+            console.log("Created Google Calendar Notes Task!");
+            return { status: 200, message: "Success!" };
+          } catch (err) {
+            console.error(
+              "Failed to create Google Notes event in createNotesForEvent() for a second time",
+              err
+            );
+          }
+        } else {
+          console.error(
+            "Call to create Google Notes event in createNotesForEvent() failed for some other reason than 401",
+            err
+          );
+        }
+      }
     }
   }
 
