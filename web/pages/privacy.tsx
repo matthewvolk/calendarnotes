d525e78824e9a5a5150d4c94ec68903099bfa8ce
Matthew Volk <volkmattj@gmail.com>
2021-05-31 09:59:42 -0500

    add privacy policy and terms

--- web/pages/privacy.tsx
+++ web/pages/privacy.tsx
@@ -13,6 +13,359 @@ export default function Privacy() {
         />
       </Head>
       <Headline>Privacy Policy</Headline>
+      <div
+        style={{
+          maxWidth: "800px",
+          margin: "0 auto",
+          fontSize: "1.15rem",
+        }}
+      >
+        <h2 style={{ fontSize: "2rem" }}>1. Purpose of this Privacy Policy</h2>
+        <p style={{ fontWeight: 300 }}>
+          CalendarNotes.io is committed to protecting your privacy. We have
+          prepared this Privacy Policy to describe to you our practices
+          regarding the Personal Data (as defined below) we collect from users
+          of our website located at CalendarNotes.io and in connection with our
+          CalendarNotes product and service (the "Product"). In addition, this
+          Privacy Policy tells you about your privacy rights and how the law
+          protects you.
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          This website and our Product is not intended for children and we do
+          not knowingly collect data relating to children.
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>2. Controller and Contact Details</h2>
+        <p style={{ fontWeight: 300 }}>
+          CalendarNotes.io (collectively referred to as CalendarNotes,” “we,”
+          “us” or “our” in this Privacy Policy) is the controller of Personal
+          Data submitted in accordance with this Privacy Policy and is
+          responsible for that Personal Data. If you have any questions about
+          this Privacy Policy, including any requests to exercise your legal
+          rights, please contact Matt Volk at matt@calendarnotes.io.
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>3. Types Of Data We Collect</h2>
+        <p style={{ fontWeight: 300 }}>
+          We collect Personal Data and Anonymous Data from you when you visit
+          our site, when you send us information or communications, and when you
+          use our Product. "Personal Data" means data that identifies, relates
+          to, describes, can be used to contact, or could reasonably be linked
+          directly or indirectly to you, including, for example, identifiers
+          such as your real name, unique personal identifier, online identifier,
+          Internet Protocol (IP) address, email address, account name, or other
+          similar identifiers; commercial information, including records of
+          products or services obtained, or considered, or other purchasing or
+          consuming histories or tendencies; Internet or other electronic
+          network activity information, including, but not limited to, browsing
+          history, search history, and information regarding your interaction
+          with an Internet website, application, or advertisement; and any other
+          non-public information about you that is associated with or linked to
+          any of the foregoing data. "Anonymous Data" means data that is not
+          associated with or linked to your Personal Data; Anonymous Data does
+          not permit the identification of individual persons. We do not collect
+          any Special Categories of Personal Data about you (this includes
+          details about your race or ethnicity, religious or philosophical
+          beliefs, sex life, sexual orientation, political opinions, trade union
+          membership, information about your health and genetic and biometric
+          data). For details about the precise data points we collect and the
+          categories of sources of such collection, please see Section 4 below.
+        </p>
+        <h3 style={{ fontSize: "1.5rem" }}>
+          3.1 Personal Data Your Provide to Us
+        </h3>
+        <p style={{ fontWeight: 300 }}>
+          We collect Personal Data from you, such as your first and last name,
+          e-mail, and profile picture when you use the Product or create an
+          account to log in to our web application. We also retain information
+          on your behalf, such as files and messages that you store using your
+          account (Internet electronic activity). If you provide us feedback or
+          contact us via e-mail, we will collect your name and e-mail address
+          (identifiers), as well as any other content included in the e-mail, in
+          order to send you a reply, and any information that you submit to us,
+          such as a resume (professional or employment related information). We
+          also collect other types of Personal Data that you provide to us
+          voluntarily, such as operating system and version, Product version
+          numbers, and other requested information if you contact us via e-mail
+          regarding support for the Product (commercial information or
+          Internet/electronic activity). We may also collect Personal Data, such
+          as demographic information, from you via the Product or at other
+          points in our website that state that Personal Data is being
+          collected.
+        </p>
+        <h3 style={{ fontSize: "1.5rem" }}>
+          3.2 Personal Data Collected via Technology
+        </h3>
+        <p style={{ fontWeight: 300 }}>
+          To make our website and Product1 more useful to you, our servers
+          (which may be hosted by a third party service provider, currently
+          Digital Ocean as of May 31st, 2021) collect Personal Data from you,
+          including browser type, operating system, IP address (a number that is
+          automatically assigned to your computer when you use the Internet,
+          which may vary from session to session), domain name, and/or a
+          date/time stamp for your visit (identifiers, commercial information or
+          Internet/electronic activity).
+        </p>
+        <h3 style={{ fontSize: "1.5rem" }}>
+          3.3 Personal Data Collected via Cookies
+        </h3>
+        <p style={{ fontWeight: 300 }}>
+          3.3 Personal Data Collected Via Cookies. We also use Cookies (as
+          defined below) and navigational data like Uniform Resource Locators
+          (URL) to gather information regarding the date and time of your visit
+          and the solutions and information for which you searched and which you
+          viewed (Internet/electronic activity). Currently, CalendarNotes does
+          not log this data to any database outside of Google Analytics.
+          "Cookies" are small pieces of information that a website sends to your
+          computer’s hard drive while you are viewing a web site. We may use
+          both session Cookies (which expire once you close your web browser)
+          and persistent Cookies (which stay on your computer until you delete
+          them) to provide you with a more personal and interactive experience
+          on our website. Persistent Cookies can be removed by following
+          Internet browser help file directions. You may choose to refuse or
+          disable Cookies via the settings on your browser, however by doing so,
+          some areas of our website may not work properly.
+        </p>
+        <h3 style={{ fontSize: "1.5rem" }}>
+          3.4 Personal and/or Anonymous Data Collected By Third Parties
+        </h3>
+        <p style={{ fontWeight: 300 }}>
+          (a) All of the data we collect about you is stored in secure virtual
+          private servers managed by{" "}
+          <a
+            style={{ textDecoration: "underline" }}
+            href="https://www.mongodb.com/"
+            target="_blank"
+          >
+            MongoDB
+          </a>{" "}
+          and encrypted via MongoDB's security protocols. Here are links to
+          MongoDB's{" "}
+          <a
+            style={{ textDecoration: "underline" }}
+            href="https://www.mongodb.com/legal/terms-of-use"
+            target="_blank"
+          >
+            terms of service
+          </a>{" "}
+          and{" "}
+          <a
+            style={{ textDecoration: "underline" }}
+            href="https://www.mongodb.com/legal/privacy-policy"
+            target="_blank"
+          >
+            privacy policy
+          </a>
+          .
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          (b) Our provision of a link to any other website or location is for
+          your convenience and does not signify our endorsement of such other
+          website or location or its contents. When you click on such a link,
+          you will leave our site and go to another site. During this process,
+          another entity may collect Personal Data or Anonymous Data from you.
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          (c) We have no control over, do not review, and cannot be responsible
+          for, outside websites or their content. Please be aware that the terms
+          of this Privacy Policy do not apply to these outside websites or
+          content, or to any collection of data after you click on links to such
+          outside websites. You may determine whether or not we are responsible
+          for the content of a website by reviewing the URL and confirming the
+          ownership of the applicable domain by means of a service such as
+          DNSstuff.com.
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>4. Use Of Your Data</h2>
+        <h3 style={{ fontSize: "1.5rem" }}>4.1 General Use</h3>
+        <p style={{ fontWeight: 300 }}>
+          In general, Personal Data you submit to us is used either to respond
+          to requests that you make, or to aid us in serving you better. We use
+          your Personal Data in the ways set out in the table below where we
+          also detail the legal bases we rely on to do so:
+        </p>
+        <table>
+          <tr>
+            <th>Purpose/Activity</th>
+            <th>Type of Data</th>
+            <th>Basis for Collecting This Data</th>
+          </tr>
+          <tr>
+            <td>
+              To register you as a new customer and provide administration of
+              our website and Products
+            </td>
+            <td>
+              Identifiers such as first name, last name, profile picture, and
+              email address
+            </td>
+            <td>Necessary for our legitimate interests</td>
+          </tr>
+          <tr>
+            <td>
+              To manage our relationship with you which will include: <br />
+              (a) Notifying you about changes to our terms or privacy policy{" "}
+              <br />
+              (b) Asking you to leave a review or take a survey
+            </td>
+            <td>Identifiers, Contact Information, Profile Information</td>
+            <td>Required to reach out to you for communication</td>
+          </tr>
+        </table>
+        <h2 style={{ fontSize: "2rem" }}>
+          5. Disclosure Of Your Personal Data
+        </h2>
+        <h3 style={{ fontSize: "1.5rem" }}>
+          5.1 Disclosure to Affiliates and Third Party Service Providers
+        </h3>
+        <p style={{ fontWeight: 300 }}>
+          We do not share data with other companies under our common control
+          ("Affiliates"). By choosing to not monetize this Product, we
+          understand that selling user data is one way to monetize a product
+          that is free of charge to you, the User. CalendarNotes does not sell
+          your data to any third party company or affiliate.
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>
+          6. Your Legal Rights Regarding Your Personal Data
+        </h2>
+        <p style={{ fontWeight: 300 }}>
+          Under certain circumstances, you may have rights under applicable data
+          protection laws in relation to your Personal Data.
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          Where applicable, you may have the right to:
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          Request information about how we collect, process, use and share your
+          Personal Data (commonly known as a “right to know request”).
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          <b>Request access</b> to your Personal Data (commonly known as a “data
+          subject access request”). This enables you to receive a copy of the
+          Personal Data we hold about you and to check that we are lawfully
+          processing it.
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          <b>Request correction</b> of the Personal Data that we hold about you.
+          This enables you to have any incomplete or inaccurate data we hold
+          about you corrected, though we may need to verify the accuracy of the
+          new data you provide to us.
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          <b>Request erasure</b> of your Personal Data (commonly known as a
+          “request to be forgotten”). This enables you to ask us to delete or
+          remove Personal Data. You also have the right to ask us to delete or
+          remove your Personal Data where you have successfully exercised your
+          right to object to processing (see below), where we may have processed
+          your information unlawfully or where we are required to erase your
+          Personal Data to comply with local law. Note, however, that we may not
+          always be able to comply in full with your request of erasure for
+          specific legal reasons which will be notified to you, if applicable,
+          at the time of your request.
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          <b>Object to processing</b> of your Personal Data where we are relying
+          on a legitimate interest (or those of a third party) and there is
+          something about your particular situation which makes you want to
+          object to processing on this ground as you feel it impacts on your
+          fundamental rights and freedoms. You also have the right to object
+          where we are processing your Personal Data for direct marketing
+          purposes. In some cases, we may demonstrate that we have compelling
+          legitimate grounds to process your information which override your
+          rights and freedoms.
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          <b>Request restriction</b> of processing of your Personal Data. This
+          enables you to ask us to suspend the processing of your Personal Data
+          in the following scenarios: (a) if you want us to establish the data’s
+          accuracy; (b) where our use of the data is unlawful but you do not
+          want us to erase it; (c) where you need us to hold the data even if we
+          no longer require it as you need it to establish, exercise or defend
+          legal claims; or (d) you have objected to our use of your data but we
+          need to verify whether we have overriding legitimate grounds to use
+          it.
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          <b>Request the transfer</b> of your Personal Data to you or to a third
+          party. We will provide to you, or a third party you have chosen, your
+          Personal Data in a structured, commonly used, machine-readable format.
+          Note that this right only applies to automated information which you
+          initially provided consent for us to use or where we used the
+          information to perform a contract with you.
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          <b>Withdraw consent</b> at any time where we are relying on consent to
+          process your Personal Data. However, this will not affect the
+          lawfulness of any processing carried out before you withdraw your
+          consent. If you withdraw your consent, we may not be able to provide
+          certain products or services to you. We will advise you if this is the
+          case at the time you withdraw your consent.
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          If you wish to exercise any of the above rights please contact
+          matt@calendarnotes.io
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          You will not have to pay a fee to access your Personal Data (or to
+          exercise any of the other rights), and CalendarNotes.io does not
+          discriminate based on whether you choose to exercise your choice and
+          rights. We will not, based on your exercise of rights, deny our
+          Products to you, charge you different rates, provide a different level
+          or quality of Products to you, or suggest that you may receive such
+          different treatment. However, we may charge a reasonable fee if your
+          request is clearly unfounded, repetitive or excessive.
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>7. Security Of Your Personal Data</h2>
+        <p style={{ fontWeight: 300 }}>
+          Unfortunately, the transmission of information via the internet is not
+          completely secure. Although we are committed to protecting your
+          Personal Data, we cannot guarantee the security of your information
+          transmitted to our site; any transmission is at your own risk. Once we
+          have received your information, we use a variety of industry-standard
+          security technologies and procedures to help protect your Personal
+          Data from unauthorized access, use, or disclosure. We have put in
+          place procedures to deal with any suspected Personal data breach and
+          will notify you and any applicable regulator of a breach where
+          required to do so.
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>8. Data Retention</h2>
+        <p style={{ fontWeight: 300 }}>
+          We will only retain your Personal Data for as long as necessary to
+          fulfill the purposes we collected it for, including for the purposes
+          of satisfying any legal, accounting, or reporting requirements.
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          To determine the appropriate retention period for Personal Data, we
+          consider the amount, nature, and sensitivity of the Personal Data, the
+          potential risk of harm from unauthorised use or disclosure of your
+          Personal Data, the purposes for which we process your Personal Data
+          and whether we can achieve those purposes through other means, and the
+          applicable legal requirements.
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>9. Dispute Resolution</h2>
+        <p style={{ fontWeight: 300 }}>
+          10. Dispute Resolution. If you believe that we have not adhered to
+          this Privacy Policy, please contact us by e-mail at
+          matt@calendarnotes.io. We will do our best to address your concerns.
+          If you feel that your complaint has been addressed incompletely, we
+          invite you to let us know for further investigation. If we are unable
+          to reach a resolution to the dispute, we will settle the dispute
+          exclusively under the rules of the American Arbitration Association.
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>10. Accessibility</h2>
+        <p style={{ fontWeight: 300 }}>
+          We are committed to making our products and services accessible to
+          everyone. If you need help with your accessibility-related requests
+          and other servicing needs please contact us at matt@calendarnotes.io
+        </p>
+        <p
+          style={{
+            fontStyle: "italic",
+            fontSize: "1.05rem",
+            marginTop: "3rem",
+            fontWeight: 300,
+          }}
+        >
+          This Privacy Policy was last revised on 31 May 2021.
+        </p>
+      </div>
     </Layout>
   );
 }
--- web/pages/terms.tsx
+++ web/pages/terms.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Headline from "../components/headline";
 import Layout from "../components/layout";
 
@@ -13,6 +14,125 @@ export default function Terms() {
         />
       </Head>
       <Headline>Terms of Service</Headline>
+      <div
+        style={{
+          maxWidth: "800px",
+          margin: "0 auto",
+          fontSize: "1.15rem",
+        }}
+      >
+        <p style={{ fontWeight: 300 }}>
+          Please read these terms of use ("terms of use", "agreement") carefully
+          before using the CalendarNotes.io website/application ("website",
+          "service", "application") operated by CalendarNotes.io ("us", "we",
+          "our").
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>Conditions of Use</h2>
+        <p style={{ fontWeight: 300 }}>
+          By ysing this website, you certify that you have read and reviewed
+          this agreement and that you agree to comply with its terms. If you do
+          not want to be bound by the terms of this agreement, you are advised
+          to leave this website accordingly. CalendarNotes.io only grants use
+          and access of this website, its products, and its services to those
+          who have accepted its terms.
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>Privacy Policy</h2>
+        <p style={{ fontWeight: 300 }}>
+          Before you continue using our website, we advise you to read our{" "}
+          <Link href="/privacy">
+            <a style={{ textDecoration: "underline" }}>privacy policy</a>
+          </Link>
+          regarding our user data collection. It will help you better understand
+          our practices.
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>Age Restriction</h2>
+        <p style={{ fontWeight: 300 }}>
+          You must be at least 18 (eighteen) years of age before you can use
+          this website. By using this website, you warrant that you are at least
+          18 years of age and you may legally adhere to this agreement.
+          CalendarNotes.io assumes no responsibility for liabilities related to
+          age misinterpretation.
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>Intellectual Property</h2>
+        <p style={{ fontWeight: 300 }}>
+          You agree that all materials, products, and services provided on this
+          website are the property of CalendarNotes.io, its affiliates,
+          directors, officers, employees, agents, suppliers, or licensors
+          including all copyrights, trade secrets, trademarks, patents, and
+          other intellectual property. You also agree that you will not
+          reproduce or redistribute CalendarNotes.io's intellectual property in
+          any way, including electronic, digital, or new trademark
+          registrations.
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          You grant CalendarNotes a royalty-free and non-exclusive license to
+          display, use, copy, transmit, and broadcast the content you upload and
+          publish. For issues regarding intellectual property claims, you should
+          contact matt@calendarnotes.io in order to come to an agreement.
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>User Accounts</h2>
+        <p style={{ fontWeight: 300 }}>
+          As a user of this website, you may be asked to register with us and
+          provide private information. You are responsible for ensuring the
+          accuracy of this information, and you are responsible for maintaining
+          the safety and security of your identifying information. You are also
+          responsible for all activities that occur under your account or
+          password.
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          If you think there are any possible issues regarding the security of
+          your account on the website, inform us immediately so we may address
+          it accordingly.
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          We reserve all rights to terminate user accounts, edit or remove
+          content, and cancel orders in their sole discretion.
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>Applicable Law</h2>
+        <p style={{ fontWeight: 300 }}>
+          By visiting this website, you agree that the laws of the United
+          States, without regard to principles of conflict laws, will govern
+          these terms and conditions, or any dispute of any sort that might come
+          between CalendarNotes and you, or its business partners and
+          associates.
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>Disputes</h2>
+        <p style={{ fontWeight: 300 }}>
+          Any dispute related in any way to your visit to this website or to
+          products you purchase from us shall be arbitrated by state or federal
+          courts and you consent to exclusive jurisdiction and venue of such
+          courts.
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>Idemnification</h2>
+        <p style={{ fontWeight: 300 }}>
+          You agree to indemnify CalendarNotes and its affiliates and hold
+          CalendarNotes harmless against legal claims and demands that may arise
+          from your use or misuse of our services. We reserve the right to
+          select our own legal counsel.
+        </p>
+        <h2 style={{ fontSize: "2rem" }}>Limitation on Liability</h2>
+        <p style={{ fontWeight: 300 }}>
+          CalendarNotes is not liable for any damages that may occur to you as a
+          result of your misuse of our website.
+        </p>
+        <p style={{ fontWeight: 300 }}>
+          CalendarNotes reserves the right to edit, modify, and change this
+          agreement at any time. We shall let our users know of these changes
+          through electronic mail. This agreement is an understanding between
+          CalendarNotes and the user, and this supersedes and replaces all prior
+          agreements regarding the use of this website.
+        </p>
+        <p
+          style={{
+            fontStyle: "italic",
+            fontSize: "1.05rem",
+            marginTop: "3rem",
+            fontWeight: 300,
+          }}
+        >
+          These Terms of Service were last revised on 31 May 2021.
+        </p>
+      </div>
     </Layout>
   );
 }
